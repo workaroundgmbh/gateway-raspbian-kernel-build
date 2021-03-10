a8426337bb38cf9d1f8c8c8ffeaf98dfe97dc67d
Arthur Crepin-Leblond <arthur.crepin@proglove.de>
2021-03-10 11:05:33 +0100

    feat(build): Add support for different boards

--- scripts/copy_to_sdcard.sh
+++ scripts/copy_to_sdcard.sh
@@ -5,7 +5,7 @@ SDCARD_MOUNT_BOOT=${SDCARD_MOUNT_BOOT:-/media/$USERNAME/boot}
 SDCARD_MOUNT_ROOT=${SDCARD_MOUNT_ROOT:-/media/$USERNAME/rootfs}
 STAGE=${1:-.}
 
-cp $STAGE/boot/zImage ${SDCARD_MOUNT_BOOT}/kernel.img
-cp $STAGE/boot/dts/*.dtb ${SDCARD_MOUNT_BOOT}/
-cp $STAGE/boot/dts/overlays/*.dtb* ${SDCARD_MOUNT_BOOT}/overlays/
-cd $STAGE && sudo cp -r --no-dereference lib ${SDCARD_MOUNT_ROOT}/
+cp -v $STAGE/boot/zImage ${SDCARD_MOUNT_BOOT}/${KERNEL}.img
+cp -v $STAGE/boot/dts/*.dtb ${SDCARD_MOUNT_BOOT}/
+cp -v $STAGE/boot/dts/overlays/*.dtb* ${SDCARD_MOUNT_BOOT}/overlays/
+cd $STAGE && sudo cp -v -r --no-dereference lib ${SDCARD_MOUNT_ROOT}/
